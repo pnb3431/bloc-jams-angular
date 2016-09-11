695088d28cd4a97977a8af5b888d7d2070405755
pnb3431 <pete.bailen@gmail.com>
2016-09-11 07:58:12 -0500

    assignment 11 complete

--- app/scripts/filters/timecode.js
+++ app/scripts/filters/timecode.js
@@ -8,18 +8,11 @@
      		}
      		
             var wholeSeconds = Math.floor(seconds);
-            var minutes = Math.floor(wholeSeconds / 60);
-            var remainingSeconds = wholeSeconds % 60;
+            var timer = buzz.toTimer(wholeSeconds);
  
-            var output = minutes + ':';
+            
  
-            if (remainingSeconds < 10) {
-                output += '0';   
-            }
- 
-            output += remainingSeconds;
- 
-            return output;
+            return timer;
         };
     }
  
